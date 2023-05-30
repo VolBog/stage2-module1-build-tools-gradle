1d725cf357faedd9b4e9cbfe30662095a8efbeae
volbog <volbog93@gmail.com>
2023-05-30 21:21:58 +0200

    Finish all tasks

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (NumberUtils.isCreatable(str)) {
+            double number = NumberUtils.createDouble(str);
+            return number > 0;
+        }
+        return false;
     }
 }
